f9869ec6e2a44d49f70984c9c957dc10dcce6099
Giovanny Salcedo <giovanny.salcedo@symplifica.com>
2023-02-28 23:37:15 -0500

    views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy] 
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   
   def index
     @post = Post.where(user_id: params[:user_id])
