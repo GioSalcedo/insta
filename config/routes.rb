dbee2d235c342f99cbc3d6e995dc59f0d5e587b3
Giovanny Salcedo <giovanny.salcedo@symplifica.com>
2023-03-10 06:11:22 -0500

    views and like

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :comments
+  resources :posts, only: [:index, :new, :create]
+  resources :comments, only: [:create, :destroy]
+  resources :likes, only: [:create, :destroy]
   devise_for :users
   get '/my_profile', to: 'profiles#show'
   root to: "posts#index"
